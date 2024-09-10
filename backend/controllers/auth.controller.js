ade1fa675ea1a87e4e851d3f6c969192e063707d
Muhammad Moinuddin <moinuddinilyas786@gmail.com>
2024-09-10 14:01:03 +0500

    Basic project and auth routes setup
    Signed-off-by: Muhammad Moinuddin <moinuddinilyas786@gmail.com>

---
+++ backend/controllers/auth.controller.js
@@ -0,0 +1,9 @@
+export const signup = (req, res) => {
+    console.log("signupUsers")
+}
+export const login = (req, res) => {
+    console.log("loginUsers")
+}
+export const logout = (req, res) => {
+    console.log("logoutUsers")
+}
