842e09ad55d9ce9684b7afe6e47dcd7fe24420e5
Muhammad Moinuddin <moinuddinilyas786@gmail.com>
2024-09-11 11:56:11 +0500

    Added sendMessage, route protection middleware
    Signed-off-by: Muhammad Moinuddin <moinuddinilyas786@gmail.com>

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,9 @@
 import express from "express";
 import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
+
 import authRoutes from "./routes/auth.routes.js";
+import messageRoutes from "./routes/message.routes.js";
 import connectToMongoDB from "./db/connectToMongoDB.js";
 const app = express();
 
@@ -9,8 +12,10 @@ dotenv.config();
 const PORT = process.env.PORT || 5000;
 
 app.use(express.json()); //to parse the incoming request
-app.use("/api/auth", authRoutes);
+app.use(cookieParser());
 
+app.use("/api/auth", authRoutes);
+app.use("/api/messages", messageRoutes);
 
 
 app.listen(PORT, () => {
